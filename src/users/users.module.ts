73de4d95ff74d3e740813228d6bb5cfcc814c9e3
Mperwe <frankmperwe@gmail.com>
2024-02-09 23:10:39 +0100

    add user controller & service user provider

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,9 +2,10 @@
 
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
 
 @Module({
     controllers: [UsersController],
-    providers: []
+    providers: [UsersService]
 })
 export class UsersModule {}
