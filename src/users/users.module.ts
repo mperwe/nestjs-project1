fd798d007aa9888727abfc4105898e74d1a89a9d
Mperwe <frankmperwe@gmail.com>
2024-02-09 23:07:49 +0100

    add user module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,6 +1,10 @@
 // users module
 
 import { Module } from '@nestjs/common';
+import { UsersController } from './users.controller';
 
-@Module({})
+@Module({
+    controllers: [UsersController],
+    providers: []
+})
 export class UsersModule {}
